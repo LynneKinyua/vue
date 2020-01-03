f0732bd55aa0f8f528a61772dc09b4bf4f552cc1
LynneKinyua <58708074+LynneKinyua@users.noreply.github.com>
2020-01-03 20:42:39 +0300

    second trial_ redo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
-   <Skills/>
-  
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <router-view />
   </div>
 </template>
 
 <script>
 import Skills from './components/Skills.vue'
-
 export default {
   name: 'app',
   components: {
@@ -33,5 +36,16 @@ body, html {
 #app {
     width: 50%;
 }
-
+nav {
+  padding: 20px 20px 20px 0;
+}
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  font-weight: bold;
+  margin-right: 15px;
+}
 </style>
